4210b63e095e349b551445c0a33a5cfc3a9ebb90
ghdubois <38192028+ghdubois@users.noreply.github.com>
2023-12-26 13:58:01 -0500

    Update cis_hardening_ubuntu22.sh

--- cis_hardening_ubuntu22.sh
+++ cis_hardening_ubuntu22.sh
@@ -325,12 +325,12 @@ check_var_partition() {
     fi
 }
 
-# Function to set correct mount options for /var
-configure_var_partition() {
-    echo "<device> /var <fstype> defaults,rw,nosuid,nodev,relatime 0 0" >> /etc/fstab
-    mount -o remount /var
-    echo "Configured mount options for /var."
-}
+# # Function to set correct mount options for /var
+# configure_var_partition() {
+#     echo "<device> /var <fstype> defaults,rw,nosuid,nodev,relatime 0 0" >> /etc/fstab
+#     mount -o remount /var
+#     echo "Configured mount options for /var."
+# }
 
 # Function to ensure /var/tmp is a separate partition
 check_var_tmp_partition() {
@@ -341,12 +341,12 @@ check_var_tmp_partition() {
     fi
 }
 
-# Function to set correct mount options for /var/tmp
-configure_var_tmp_partition() {
-    echo "<device> /var/tmp <fstype> defaults,rw,nosuid,nodev,noexec,relatime 0 0" >> /etc/fstab
-    mount -o remount /var/tmp
-    echo "Configured mount options for /var/tmp."
-}
+# # Function to set correct mount options for /var/tmp
+# configure_var_tmp_partition() {
+#     echo "<device> /var/tmp <fstype> defaults,rw,nosuid,nodev,noexec,relatime 0 0" >> /etc/fstab
+#     mount -o remount /var/tmp
+#     echo "Configured mount options for /var/tmp."
+# }
 
 
 
@@ -359,11 +359,11 @@ check_var_tmp_partition_options() {
     fi
 }
 
-configure_var_tmp_partition_options() {
-    echo "<device> /var/tmp <fstype> defaults,rw,nosuid,nodev,noexec,relatime 0 0" >> /etc/fstab
-    mount -o remount /var/tmp
-    echo "Configured mount options for /var/tmp."
-}
+# configure_var_tmp_partition_options() {
+#     echo "<device> /var/tmp <fstype> defaults,rw,nosuid,nodev,noexec,relatime 0 0" >> /etc/fstab
+#     mount -o remount /var/tmp
+#     echo "Configured mount options for /var/tmp."
+# }
 
 # Functions for /var/log partition
 check_var_log_partition_options() {
@@ -374,11 +374,11 @@ check_var_log_partition_options() {
     fi
 }
 
-configure_var_log_partition_options() {
-    echo "<device> /var/log <fstype> defaults,rw,nosuid,nodev,noexec,relatime 0 0" >> /etc/fstab
-    mount -o remount /var/log
-    echo "Configured mount options for /var/log."
-}
+# configure_var_log_partition_options() {
+#     echo "<device> /var/log <fstype> defaults,rw,nosuid,nodev,noexec,relatime 0 0" >> /etc/fstab
+#     mount -o remount /var/log
+#     echo "Configured mount options for /var/log."
+# }
 
 # Functions for /var/log/audit partition
 check_var_log_audit_partition_options() {
@@ -389,11 +389,11 @@ check_var_log_audit_partition_options() {
     fi
 }
 
-configure_var_log_audit_partition_options() {
-    echo "<device> /var/log/audit <fstype> defaults,rw,nosuid,nodev,noexec,relatime 0 0" >> /etc/fstab
-    mount -o remount /var/log/audit
-    echo "Configured mount options for /var/log/audit."
-}
+# configure_var_log_audit_partition_options() {
+#     echo "<device> /var/log/audit <fstype> defaults,rw,nosuid,nodev,noexec,relatime 0 0" >> /etc/fstab
+#     mount -o remount /var/log/audit
+#     echo "Configured mount options for /var/log/audit."
+# }
 
 # Function to configure rsyslog default file permissions
 configure_rsyslog_permissions() {
