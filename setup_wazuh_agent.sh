586ca7f53c7a9d2833d2c43627210f9dee65dab0
ghdubois <38192028+ghdubois@users.noreply.github.com>
2024-01-04 17:28:45 -0500

    Update setup_wazuh_agent.sh

--- setup_wazuh_agent.sh
+++ setup_wazuh_agent.sh
@@ -4,7 +4,7 @@ echo "deb [signed-by=/usr/share/keyrings/wazuh.gpg] https://packages.wazuh.com/4
 
 apt-get update &&
 
-WAZUH_MANAGER=$0 apt-get install wazuh-agent &&
+WAZUH_MANAGER="192.168.5.31" apt-get install wazuh-agent &&
 
 systemctl daemon-reload &&
 systemctl enable wazuh-agent &&
