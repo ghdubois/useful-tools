4b3c45ca4cbbf20c5dc66b4e837b188db5985677
ghdubois <38192028+ghdubois@users.noreply.github.com>
2024-01-07 04:03:36 -0500

    Update set_dns_fw.sh

--- set_dns_fw.sh
+++ set_dns_fw.sh
@@ -1,3 +1,5 @@
+
+
 #!/bin/bash
 
 # Reset UFW to default settings
@@ -8,14 +10,24 @@ ufw default deny incoming
 ufw default allow outgoing
 
 # Allow SSH traffic from specific IP address
-ufw allow from 10.1.1.2 to any port 22
+ufw allow from 192.168.2.2 to any port 22
 
 # Allow DNS traffic from local IP range 10.1.123.0/28
-ufw allow from 10.1.123.0/28 to any port 53
+ufw allow from 192.168.0.0/16 to any port 53
 
 # Allow HTTP and HTTPS traffic for Pi-hole web interface
-ufw allow from 10.1.1.2 to any port 80
-ufw allow from 10.1.1.2 to any port 443
+ufw allow from 192.168.2.2 to any port 80
+ufw allow from 192.168.2.2 to any port 443
+
+# # Allow SSH traffic from specific IP address
+# ufw allow from 10.1.1.2 to any port 22
+
+# # Allow DNS traffic from local IP range 10.1.123.0/28
+# ufw allow from 10.1.123.0/28 to any port 53
+
+# # Allow HTTP and HTTPS traffic for Pi-hole web interface
+# ufw allow from 10.1.1.2 to any port 80
+# ufw allow from 10.1.1.2 to any port 443
 
 # Enable UFW
 ufw --force enable
