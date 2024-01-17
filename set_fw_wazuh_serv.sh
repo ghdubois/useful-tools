93338e6a7b154d8888080de1f62cd7611c83f5ab
ghdubois <38192028+ghdubois@users.noreply.github.com>
2024-01-17 00:48:52 -0500

    Update set_fw_wazuh_serv.sh

--- set_fw_wazuh_serv.sh
+++ set_fw_wazuh_serv.sh
@@ -7,12 +7,11 @@ ufw default deny incoming
 ufw default allow outgoing
 
 # Allow SSH (optional, adjust the port if you use a non-standard one)
-ufw allow 22
+# ufw allow 22
 
 # Allow Wazuh server ports
 # Replace these with the ports you have configured for Wazuh
 ufw allow 1514/tcp   # For Wazuh agent communication
-ufw allow 1515/tcp   # For agent registration
 ufw allow 514/udp    # For syslog
 ufw allow 55000/tcp  # For Wazuh API
 
